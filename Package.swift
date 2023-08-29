4262b2e85ab5c86ad275f43350d3f5d9641f36c7
Alexander Shepelenok <alexander.shepelenok@gmail.com>
2023-08-29 15:33:00 +0200

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SeatGeekEMEASDK",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "SeatGeekEMEASDK",
+            targets: ["SeatGeekEMEASDK"]),
+    ],
+    targets: [
+        .binaryTarget(name: "SeatGeekEMEASDK",
+                      url: "https://seatgeek.jfrog.io/artifactory/emea-sdk-ios/v0.0.1/SeatGeekEMEASDK.xcframework.zip",
+                      checksum: "e9e1dab0e0be753bda2739ceba7246f08b6f22e5c080503884c6cc587bc51559"),
+    ]
+)
