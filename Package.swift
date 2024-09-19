336d9874187e70ed5b96bd228b964472a4fac824
Aleksandr Shepelenok <ashepelenok@seatgeek.com>
2024-09-19 12:45:43 +0200

    Release 1.3.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "SeatGeekEMEASDK",
-                      url: "https://seatgeek.jfrog.io/artifactory/emea-sdk-ios/v1.2.3/SeatGeekEMEASDK.xcframework.zip",
-                      checksum: "f4a3ac326e21e6ec585201926622108d99534233bc1fb88b41324ea8a341b13e"),
+                      url: "https://seatgeek.jfrog.io/artifactory/emea-sdk-ios/v1.3.0/SeatGeekEMEASDK.xcframework.zip",
+                      checksum: "026f0b1688c43d9928298d422c3187f58c6221f5c48b7955fd735693d1f3821d"),
     ]
 )
