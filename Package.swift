f83c7ffa6cf47e0e7796645e838c7c64a16c1182
siarhei.mikavoz <siarhei.mikavoz@rethinkfirst.com>
2025-05-21 12:31:13 +0200

    Release 1.4.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "SeatGeekEMEASDK",
-                      url: "https://seatgeek.jfrog.io/artifactory/emea-sdk-ios/v1.4.1/SeatGeekEMEASDK.xcframework.zip",
-                      checksum: "6f53e91817b9288fc8349ad06007455bd772716ecca90274dfce84d18b2ef9a4"),
+                      url: "https://seatgeek.jfrog.io/artifactory/emea-sdk-ios/v1.4.2/SeatGeekEMEASDK.xcframework.zip",
+                      checksum: "9fad08c2d789cedfe99146b9cbb76331216fddb679e7710e91fb8b26eba6a87d"),
     ]
 )
